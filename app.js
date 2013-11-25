4b4416dd3d16aea1680abefba07b4e61c8d094aa
Gord Lea <jgordonlea@gmail.com>
2013-11-25 09:21:48 -0800

    Cleaned up and added help

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var argv = require('optimist').argv;
 var Promise = require("promise");
 var sh = require('execSync');
 
+if (argv.help !== undefined) {
+    var help = fs.readFileSync(__dirname + "/help.txt", 'utf8');
+    console.log(help);
+    return;
+}
 
 var docker = null;
 if (argv.host !== undefined) {
-	console.log(argv.host)
 	docker = require('docker.io')({ socketPath: false, host: 'http://'+argv.host, port: '4243'});
 } else if (argv.socket !== undefined) {
 	docker = require('docker.io')({ socketPath: argv.socket });
@@ -19,22 +23,29 @@ if (argv.host !== undefined) {
 var getDockerDetails = Promise.denodeify(docker.containers.inspect);
 var options = {}; // all options listed in the REST documentation for Docker are supported.
 var templatePath = __dirname + "/nginx_template";
-console.log(templatePath)
 var template = fs.readFileSync(templatePath, 'utf8');
 var outputPath = argv.outputPath !== undefined ? argv.outputPath + '/' : "/etc/nginx/conf.d/"; 
-console.log(outputPath);
+
 docker.containers.list(options /* optional*/, function(err, containerList) {
     if (err) throw err;
     
     // console.log('Found %d containers, filtering', allContainers.length)
     var containers = _.filter(containerList, function(containerSummary) {
-    	return _.some(containerSummary.Ports, function(port) {
+        var containerId = fmtContainerId(containerSummary.Id);
+        console.log("checking container %s", containerId);
+
+    	var result = _.some(containerSummary.Ports, function(port) {
+            console.log("   has PrivatePort 80: %s", port.PrivatePort === 80);
+            console.log("   has PublicPort != undefined: %s", port.PublicPort !== undefined);
     		return port.PrivatePort === 80 && port.PublicPort !== undefined;
     	});
+        console.log("container %s will be configured: %s", containerId, result);
+        return result;
+
     });
-    var promises = []
     _.forEach(containers, function(container) {
     	docker.containers.inspect(container.Id, function(err, details) {
+            // console.dir(details);
     		var templateData = {
     			domain: details.Config.Hostname + '.' + details.Config.Domainname,
     			internal_port: details.NetworkSettings.Ports["80/tcp"][0].HostPort
@@ -42,6 +53,9 @@ docker.containers.list(options /* optional*/, function(err, containerList) {
 
     		var templateOutput = _.template(template, templateData);
     		fs.writeFileSync(outputPath + details.Config.Hostname + ".conf", templateOutput);
+            console.log("wrote %s for container %s", outputPath + details.Config.Hostname + ".conf", fmtContainerId(container.Id));
+            var code = sh.run('service nginx reload');
+            console.log("reloaded nginx config, returned %s", code);
     	});
 
     	
@@ -49,3 +63,7 @@ docker.containers.list(options /* optional*/, function(err, containerList) {
     });
 
 });
+
+function fmtContainerId(containerId) {
+    return containerId.substr(0,12);
+}
